973d1fda02a439ea6dfbfd13511121aa903938d6
ChristianBrauerJohanssen <60613177+ChristianBrauerJohanssen@users.noreply.github.com>
2022-04-27 13:45:24 +0200

    post exc 27 april

--- 3_discrete_continuous_choice/exercises/ex_ante/egm_exante.py
+++ 3_discrete_continuous_choice/exercises/ex_ante/egm_exante.py
@@ -11,15 +11,32 @@ def EGM(sol,t,par):
 
 def EGM_loop (sol,t,par):
     for i_a,a in enumerate(par.grid_a[t,:]):
+        if t+1 <= par.Tr: # no retirement in next period
+            fac = par.G*par.L[t]*par.psi_vec
+            w = par.w
+            xi = par.xi_vec
+            inv_fac = 1/fac
 
-        # Futute m and c
-        
+            # Future m and c
+            m_next = inv_fac*par.R*a+xi
+            c_next = tools.interp_linear_1d(sol.m[t+1,:],sol.c[t+1,:], m_next)
+        else: 
+            fac = par.G*par.L[t]
+            w = 1
+            xi = 1
+            inv_fac = 1/fac
+
+            # Future m and c
+            m_next = inv_fac*par.R*a+xi
+            c_next = tools.interp_linear_1d_scalar(sol.m[t+1,:],sol.c[t+1,:], m_next)
 
         # Future marginal utility
+        marg_u_next = util.marg_util(fac*c_next,par)
+        avg_marg_u_next = np.sum(w*marg_u_next)
 
-        # Currect C and m
-        #sol.c[t,i_a+1]=
-        #sol.m[t,i_a+1]=
+        # Correct C and m
+        sol.c[t,i_a+1] = util.inv_marg_util(par.beta*par.R*avg_marg_u_next,par)
+        sol.m[t,i_a+1] = a+sol.c[t,i_a+1]
 
     return sol
 
